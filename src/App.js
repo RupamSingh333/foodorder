38fa41f902d672a2b73fdd2f3634a480c8cf992c
RupamSingh333 <rupamkumar333@gmail.com>
2023-06-08 09:07:54 +0530

    Cart context implement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import Header from "../src/components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -20,13 +21,14 @@ function App() {
   ];
 
   return (
-    <Fragment>
+    
+    <CartProvider>
       {cartIsShown && <Cart items={cartItems} onCloseCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
